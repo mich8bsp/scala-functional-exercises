422cf6ec9889f0183b649df140916238b413303d
Michael Bespalov <mich8bsp@gmail.com>
2017-03-31 18:12:36 +0300

    chapter3

---
+++ src/Chapter3.scala
@@ -0,0 +1,54 @@
+import Chapter3.List.sum
+import Chapter3.List.tail
+/**
+  * Created by mich8 on 31-Mar-17.
+  */
+object Chapter3 {
+
+  sealed trait List[+A]
+
+  case object Nil extends List[Nothing]
+
+  case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+  object List {
+    def sum(ints: List[Int]): Int = ints match {
+      case Nil => 0
+      case Cons(x, xs) => x + sum(xs)
+    }
+
+    def product(ds: List[Double]): Double = ds match {
+      case Nil => 1.0
+      case Cons(0.0, _) => 0.0
+      case Cons(x, xs) => x * product(xs)
+    }
+
+    def apply[A](as: A*): List[A] =
+      if (as.isEmpty) Nil
+      else Cons(as.head, apply(as.tail: _*))
+
+//    ex 3.2
+    def tail[A](ds: List[A]): List[A] = ds match{
+      case Nil => Nil
+      case Cons(x, xs) => xs
+    }
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    val x = List(1, 2, 3, 4, 5) match {
+      case Cons(x, Cons(2, Cons(4, _))) => x
+      case Nil => 42
+      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+      case Cons(h, t) => h + sum(t)
+      case _ => 101
+    }
+
+    // ex 3.1
+    //result 3
+    println(x)
+
+    println(tail(List("a", "b", "c")))
+    println(tail(List()))
+  }
+}
